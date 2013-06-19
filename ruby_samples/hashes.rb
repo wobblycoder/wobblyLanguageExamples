04d19ff166aded42feeecc4ec3de417954b85df5
Chris Spindler <spindler.c@gmail.com>
2013-06-18 23:01:44 -0500

    more hash examples

--- ruby_samples/hashes.rb
+++ ruby_samples/hashes.rb
@@ -12,6 +12,21 @@ d.merge!({"orange" => "orange"})
 puts d.keys
 puts d.values
 
+if d.include? "apple"
+    puts "I have an apple, and it is #{ d["apple"] }."
+end
+
+if d.include? "watermelon"
+    puts "I have an watermelon, and it is #{ d["watermelon"] }."
+else
+    puts "I don't have a watermelon."
+end    
+
 puts d.index("apple")
+
+d.each do |fruit, color|
+    puts "The #{fruit} is #{color}"
+end
+
 
 
