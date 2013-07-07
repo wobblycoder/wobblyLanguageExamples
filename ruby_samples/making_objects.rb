75d7aa98e595dbc30f0ebc3485d51e45342208e2
Chris Spindler <spindler.c@gmail.com>
2013-07-07 00:06:07 -0500

    clarifying comments and showing the default attributes

--- ruby_samples/making_objects.rb
+++ ruby_samples/making_objects.rb
@@ -10,7 +10,7 @@ puts "the absolute value of x is #{x.abs}"
 s = "Hello, world!"
 puts s.upcase
 
-# what if we would like to make a new class of variables, 
+# what if we would like to make a new class (or kind) of variables, 
 # not a string or number, but representing something else?
 
 # for example, what if we wanted to keep track of a player in a game?
@@ -20,21 +20,24 @@ puts s.upcase
 # first, we need to tell Ruby where to find the definition of our class
 require_relative 'wobbly_player'
 
+# now we can ask our WobblyPlayer class to give us a new object to represent 
+# our player
+
 p1 = WobblyPlayer.new
 
 # the next few lines keep track of information about the player
-# (we call each distince piece of information an "attribute")
+# (we call each distinct piece of information an "attribute")
 
 p1.name = "Chris"     # the "name" attribute tells us the player's name
 
-p1.health = 100        # the "health" attribute tells us what % of the player's 
+p1.health = 100       # the "health" attribute tells us what % of the player's 
                       #    health is remaining
 
 p1.room = 1           # the "room" attribute, maybe the player can move from 
                       #    room to room
 
 p1.loc_x = 1       # the "loc_x" and "loc_y" attributes 
-p1.loc_y = 1       #    tells us where in the current
+p1.loc_y = 1       #    tell us where in the current
                    #    room the player is standing 
 
 # the next few lines ask the player to do some stuff.
@@ -44,4 +47,10 @@ p1.moves_left         # move the player to the left by a step
 p1.takes_damage 10    # uh-oh, the player just got hurt
 
 puts p1.status        # let's see what the player looks like now
+
+# go look in the wobbly_player.rb file to see how it works
+
+p2 = WobblyPlayer.new
+
+puts p2.status
 
